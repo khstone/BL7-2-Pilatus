6fa083de8b45d508b2ad6fce41d4016aeb57d6c1
Kevin H. Stone <khstone@slac.stanford.edu>
2018-10-08 18:19:22 -0700

    Add files via upload

---
+++ Pilatus_Calibration_setup.py
@@ -0,0 +1,7 @@
+# Useful information for calibration goes here
+data_path = "C:/SLAC/Data/SSRL/BL72/images/"
+calib_name = "b_stone_calibration_scan1_"
+csv_path = "C:/SLAC/Data/SSRL/BL72/"
+csv_name = "calibration_scan1.csv"
+db_pixel = [230, 91]  # direct beam pixel
+pix_size = 172.0  # pixel size in microns
---
+++ Pilatus_Integration_setup.py
@@ -0,0 +1,10 @@
+# Useful information for calibration goes here
+calib_path = "C:/SLAC/Data/SSRL/BL72/"
+calib_name = "calibration_scan1_calib.cal"
+
+# Information for scan to be integrated goes here
+user = "b_stone_"
+image_path = "C:/SLAC/Data/SSRL/BL72/images/"
+spec_path = "C:/SLAC/Data/SSRL/BL72/"
+spec_name = "LaB6_calibration"
+scan_number = 1
